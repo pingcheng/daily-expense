9a3287a6c5c0ff994d5cdc6552d538ca3cae3c56
Ping Cheng <ping.che@hotmail.com>
2020-08-17 10:39:02 +1000

    Update index.ts

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,13 +1,16 @@
 import Vue from 'vue'
-import Vuex from 'vuex'
+import Vuex, { StoreOptions } from 'vuex'
 import ProfileStore from "@/store/modules/ProfileStore.ts";
 import ConfigStore from "@/store/modules/ConfigStore.ts";
+import RootState from "@/store/RootState.ts";
 
 Vue.use(Vuex)
 
-export default new Vuex.Store({
+const options: StoreOptions<RootState> = {
   modules: {
     profile: ProfileStore,
     config: ConfigStore,
   }
-})
+}
+
+export default new Vuex.Store(options);
